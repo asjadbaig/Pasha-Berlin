59e8aa8fb8ee3c2ba417c68bc4a289a4f5ed600f
Muhamad Asjad Baig <asgidbaig@gmail.com>
2017-08-01 13:14:18 +0200

    added bootstrap classes

---
+++ contact.js
@@ -0,0 +1,6 @@
+(function(d, t){
+   var g = d.createElement(t),
+       s = d.getElementsByTagName(t)[0];
+   g.src = "http://www.foxyform.com/js.php?id=888610&sec_hash=6a6bf284af6&width=350px";
+   s.parentNode.insertBefore(g, s);
+}(document, "script"));
